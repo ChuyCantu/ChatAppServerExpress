44fd92b0c453472f8e2b84038b4513be141af545
Jesus Cantu <chuy_cr11@hotmail.com>
2022-07-27 18:47:54 -0500

    Add sequelize migrations test.

---
+++ config/config.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    use_env_variable: "DATABASE_URL",
+    dialect: process.env.DB_DIALECT
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: process.env.DB_DIALECT
+  }
+};
